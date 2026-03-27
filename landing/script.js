74689d16de64aff3a5eea0f194360fd2ce0b04ec
Gabrielle Cruz <gabriellefcruz207@gmail.com>
2026-03-27 17:48:43 -0300

    feat(landing): add contact form submission behavior

--- landing/script.js
+++ landing/script.js
@@ -23,3 +23,32 @@ overlay.addEventListener("click", (e) => {
         overlay.style.display = "none";
     }
 });
+
+// Mensagem de envio e apaga o formulário de contato
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Pega o formulário e mensagem pelo id
+    const form = document.getElementById("formContato");
+    const mensagem = document.getElementById("mensagem");
+
+    // Escuta quando o formulário é enviado (clicar no botão)
+    form.addEventListener("submit", function (event) {
+
+        // Impede a página de recarregar
+        event.preventDefault();
+
+        // Adiciona a classe "mostrar" e faz a mensagem aparecer
+        mensagem.classList.add("mostrar");
+
+        // Limpa os campos do formulário
+        form.reset();
+
+        // Espera 3 segundos
+        setTimeout(function () {
+
+            // Remove a classe "mostrar" e faz a mensagem sumir
+            mensagem.classList.remove("mostrar");
+        }, 3000);
+    });
+});
