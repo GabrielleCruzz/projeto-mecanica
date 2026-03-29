c3055de61c35a259e09297e65708b3d717c52cc1
Gabrielle Cruz <gabriellefcruz207@gmail.com>
2026-03-29 20:38:46 -0300

    feat(dashboard): add close button to sidebar

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,8 +1,14 @@
 const menuBtn = document.getElementById("menuBtn");
+const btnFechar = document.getElementById("btnFechar");
 const menu = document.getElementById("menu");
 
 // abrir menu
 menuBtn.addEventListener("click", () => {
     menu.classList.add("active");
     menuBtn.style.display = "none"; // esconde o botão de menu quando o menu lateral estiver aberto
+});
+
+btnFechar.addEventListener("click", () => {
+    menu.classList.remove("active");
+    menuBtn.style.display = "block"; // mostra o botão de menu quando o menu lateral estiver fechado
 });
