49597b7316276331f042105b99babac5f4a77448
Gabrielle Cruz <gabriellefcruz207@gmail.com>
2026-04-02 21:55:26 -0300

    fix(orcamento): prevent background screoll when modal is open

--- dashboard/script.js
+++ dashboard/script.js
@@ -4,14 +4,16 @@ const btnFechar = document.getElementById("btnFechar");
 
 // Modal
 
-// Abre o modal
+// Abre o modal e trava o scroll da página
 function abrirModal() {
     overlay.style.display = "flex";
+    document.body.style.overflow = "hidden";
 }
 
-// Fecha o modal
+// Fecha o modal e libera o scroll da página
 function fecharModal() {
     overlay.style.display = "none";
+    document.body.style.overflow = "auto";
 }
 
 // Eventos de abrir e fechar o modal
