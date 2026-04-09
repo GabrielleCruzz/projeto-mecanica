c360931009492bd8b4b4d67c582b8067799005be
GabrielleCruzz <gabriellefcruz207@gmail.com>
2026-04-09 18:49:33 -0300

    feat(menu-lateral): close sidebar on outside click

--- dashboard/js/menu-lateral.js
+++ dashboard/js/menu-lateral.js
@@ -13,4 +13,15 @@ function initMenuLateral() {
         menu.classList.remove("active");
         menuBtn.style.visibility = "visible"; // mostra o icone do menu quando o menu lateral estiver fechado
     });
+
+    // fechar o menu clicando fora
+    document.addEventListener("click", (event) => {
+        const foraDoMenu = !menu.contains(event.target); // !menu -> tudo que não for o menu
+        const btn = menuBtn.contains(event.target);
+
+        if (menu.classList.contains("active") && foraDoMenu && !btn) { // && !btn impede que o clique no botão de abrir o menu tbm feche ele
+            menu.classList.remove("active"); // Fecha o menu
+            menuBtn.style.visibility = "visible"; // Botão de abrir o menu aparece de novo
+        }
+    });
 }
